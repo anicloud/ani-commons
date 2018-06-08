722d76cd47074bc41b5b35da354924e104cbadfc
ebrx <ebrx@vip.qq.com>
2018-06-08 16:45:10 +0800

    #1 Add service comment #2 Code optimization

--- octopus-commons/src/main/java/com/ani/commons/octopus/servicemeta/service/AniServiceMetaService.java
+++ octopus-commons/src/main/java/com/ani/commons/octopus/servicemeta/service/AniServiceMetaService.java
@@ -19,30 +19,115 @@ import java.util.List;
  */
 public interface AniServiceMetaService {
 
+     /**
+      * <h2>Add Service.</h2>
+      * @param registerDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public void addService(AniServiceRegisterDto registerDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Update the Service information.</h2>
+      * @param updateDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public void updateServiceInfo(AniServiceUpdateDto updateDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Setting integration strategy.</h2>
+      * @param aniSSOStrategyDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public void setSSOStrategy(AniSSOStrategyRegisterDto aniSSOStrategyDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Add data synchronization Strategy.</h2>
+      * @param registerDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public void addDataSynStrategy(AniDataSynStrategyRegisterDto registerDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Delete Service.</h2>
+      * @param modelQueryDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public void removeService(ModelQueryDto modelQueryDto)throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      *<h2>Query basic information.</h2>
+      * @param modelQueryDto
+      * @return AniServiceInfoDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public AniServiceInfoDto getAniServiceById(ModelQueryDto modelQueryDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Query integration strategy.</h2>
+      * @param modelQueryDto
+      * @return AniSSOStrategy
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public AniSSOStrategy getSSOStrategyByService(ModelQueryDto modelQueryDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Query data synchronization Strategy.</h2>
+      * @param modelQueryDto
+      * @return AniDataSynStrategy
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public AniDataSynStrategy getDataSynStrategy(ModelQueryDto modelQueryDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Update Service State (1.ACTIVE/2.DISABLE/3.REMOVED).</h2>
+      * @param modelStateUpdateDto
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public void updateServiceState(ModelStateUpdateDto modelStateUpdateDto)  throws AniAuthException, AniRuleException, AniDataException ;
 
+     /**
+      * <h2>Query the list of OrgModel by orgId.</h2>
+      * @param orgModelQueryDto
+      * @return List<ServiceBriefInfoDto>
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public List<ServiceBriefInfoDto> getServiceBasicInfoByOrgId(OrgModelQueryDto orgModelQueryDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Query the list of OrgModel by nodeId.</h2>
+      * @param orgNodeModelQueryDto
+      * @return List<ServiceBriefInfoDto>
+      * @throws AniAuthException
+      * @throws AniRuleException
+      * @throws AniDataException
+      */
      public List<ServiceBriefInfoDto> getServiceBasicInfoByNodeId(OrgNodeModelQueryDto orgNodeModelQueryDto) throws AniAuthException, AniRuleException, AniDataException;
 
+     /**
+      * <h2>Query the list of Service by accountId.</h2>
+      * @param accountId
+      * @return List<AniServiceBriefInfoDto>
+      */
      public List<AniServiceBriefInfoDto> getServiceBriefInfoByAccountId(Long accountId);
-
-//     public List<AniServiceBasicInfo> getServiceBasicInfoByAccountId(Long accountId);
 
 }
