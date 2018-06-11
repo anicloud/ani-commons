600cbc4926a2acf0b1fbf77f52cfbf64dd2f2a2f
ebrx <ebrx@vip.qq.com>
2018-06-11 19:01:56 +0800

    # 继承改引用

--- octopus-commons/src/main/java/com/ani/commons/octopus/servicemeta/dto/service/AniServiceUpdateDto.java
+++ octopus-commons/src/main/java/com/ani/commons/octopus/servicemeta/dto/service/AniServiceUpdateDto.java
@@ -2,20 +2,29 @@ package com.ani.commons.octopus.servicemeta.dto.service;
 
 import com.ani.commons.octopus.objmeta.dto.model.ModelUpdateDto;
 import com.ani.commons.octopus.servicemeta.enumeration.DualAuth;
+import com.ani.utils.dto.AniDto;
 import com.ani.utils.dto.AniRequiredField;
 
 /**
  * Created by zhanglina on 18-6-5.
  */
-public class AniServiceUpdateDto extends  ModelUpdateDto {
+public class AniServiceUpdateDto extends AniDto {
     private static final long serialVersionUID = -1887695056574931466L;
 
     @AniRequiredField
     protected String serviceType;
     @AniRequiredField
     protected DualAuth dualAuth;
+    @AniRequiredField
+    protected ModelUpdateDto modelUpdateDto;
 
+    public ModelUpdateDto getModelUpdateDto() {
+        return modelUpdateDto;
+    }
 
+    public void setModelUpdateDto(ModelUpdateDto modelUpdateDto) {
+        this.modelUpdateDto = modelUpdateDto;
+    }
     public String getServiceType() {
         return serviceType;
     }
