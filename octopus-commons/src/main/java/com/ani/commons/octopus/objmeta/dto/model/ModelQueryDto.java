bd30c782da2e81dffdb0d652589df56b6e7c70fd
ebrx <ebrx@vip.qq.com>
2018-06-15 09:58:08 +0800

    #1 update a metherd

--- octopus-commons/src/main/java/com/ani/commons/octopus/objmeta/dto/model/ModelQueryDto.java
+++ octopus-commons/src/main/java/com/ani/commons/octopus/objmeta/dto/model/ModelQueryDto.java
@@ -1,6 +1,8 @@
 package com.ani.commons.octopus.objmeta.dto.model;
 
 import com.ani.commons.octopus.objmeta.domain.ObjectType;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
  * Created by zhanglina on 18-4-19.
@@ -8,7 +10,7 @@ import com.ani.commons.octopus.objmeta.domain.ObjectType;
 public class ModelQueryDto extends ModelDto {
 
     private static final long serialVersionUID = 271223444264428728L;
-
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long requestAccountId;
 
     public ModelQueryDto() {
--- octopus-commons/src/main/java/com/ani/commons/octopus/objmeta/dto/model/OrgNodeModelQueryDto.java
+++ octopus-commons/src/main/java/com/ani/commons/octopus/objmeta/dto/model/OrgNodeModelQueryDto.java
@@ -1,6 +1,8 @@
 package com.ani.commons.octopus.objmeta.dto.model;
 
 import com.ani.utils.dto.AniRequiredField;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
  * Created by zhanglina on 18-5-23.
@@ -10,6 +12,7 @@ public class OrgNodeModelQueryDto extends OrgModelOpDto {
     private static final long serialVersionUID = 2423825827389972278L;
 
     @AniRequiredField
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long nodeId;
 
     public OrgNodeModelQueryDto() {
