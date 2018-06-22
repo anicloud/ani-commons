2cfc368ee41a5dbe6c74ebefc269780d9142ba0b
ebrx <ebrx@vip.qq.com>
2018-06-22 13:32:08 +0800

    #1 single sign on

--- bus-commons/src/main/java/com/ani/commons/bus/service/dto/sso/input/ServiceSSOAuthDto.java
+++ bus-commons/src/main/java/com/ani/commons/bus/service/dto/sso/input/ServiceSSOAuthDto.java
@@ -1,39 +1,19 @@
 package com.ani.commons.bus.service.dto.sso.input;
 
+import com.ani.commons.octopus.objmeta.dto.model.ModelQueryDto;
 import com.ani.utils.dto.AniDto;
 
 /**
  * Created by zhanglina on 18-6-21.
  */
-public abstract class ServiceSSOAuthDto extends AniDto{
+public abstract class ServiceSSOAuthDto extends ModelQueryDto {
 
     private static final long serialVersionUID = -6734549774437930125L;
 
-    private Long modelId;
-//    private Long orgId;
-    private Long accountId;
-
     public ServiceSSOAuthDto(Long modelId, Long accountId) {
-        this.modelId = modelId;
-        this.accountId = accountId;
+        super(modelId, accountId);
     }
 
     public ServiceSSOAuthDto() {
-    }
-
-    public Long getModelId() {
-        return modelId;
-    }
-
-    public void setModelId(Long modelId) {
-        this.modelId = modelId;
-    }
-
-    public Long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(Long accountId) {
-        this.accountId = accountId;
     }
 }
--- bus-commons/src/main/java/com/ani/commons/bus/service/service/SSOAuthService.java
+++ bus-commons/src/main/java/com/ani/commons/bus/service/service/SSOAuthService.java
@@ -2,10 +2,19 @@ package com.ani.commons.bus.service.service;
 
 import com.ani.commons.bus.service.dto.sso.output.ServiceSSOAuthCodeDto;
 import com.ani.commons.bus.service.dto.sso.input.ServiceSSOAuthDto;
+import com.ani.utils.exception.AniDataException;
+import com.ani.utils.exception.AniRuleException;
 
 /**
  * Created by zhanglina on 18-6-21.
  */
 public interface SSOAuthService {
-    public ServiceSSOAuthCodeDto ssoAuth(ServiceSSOAuthDto serviceSSOAuthDto);
+    /**
+     * <h2>This is a single sign on method</h2>
+     * @param serviceSSOAuthDto
+     * @return loginUrl
+     * @throws AniRuleException
+     * @throws AniDataException
+     */
+    public ServiceSSOAuthCodeDto ssoAuth(ServiceSSOAuthDto serviceSSOAuthDto) throws AniRuleException, AniDataException;
 }
