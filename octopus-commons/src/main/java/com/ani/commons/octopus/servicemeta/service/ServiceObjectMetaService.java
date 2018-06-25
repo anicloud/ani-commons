666deb1c47edeec78b9b1efc3b270402e465ef14
zhanglina <xiaojiang1129@github.com>
2018-06-25 13:25:21 +0800

    add serviceObject service

--- octopus-commons/src/main/java/com/ani/commons/octopus/servicemeta/service/ServiceObjectMetaService.java
+++ octopus-commons/src/main/java/com/ani/commons/octopus/servicemeta/service/ServiceObjectMetaService.java
@@ -3,13 +3,14 @@ package com.ani.commons.octopus.servicemeta.service;
 import com.ani.commons.octopus.objmeta.dto.object.ObjectQueryDto;
 import com.ani.commons.octopus.objmeta.dto.object.ObjectRegisterDto;
 import com.ani.commons.octopus.servicemeta.domain.object.ServiceObject;
+import com.ani.commons.octopus.servicemeta.dto.service.Object.ServiceObjectRegisterDto;
 
 /**
  * Created by zhanglina on 18-6-1.
  */
 public interface ServiceObjectMetaService {
 
-    public void registerObject(ObjectRegisterDto objectRegisterDto);
+    public void registerObject(ServiceObjectRegisterDto objectRegisterDto);
 
     public ServiceObject getServiceObject(ObjectQueryDto objectQueryDto);
 
