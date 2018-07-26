425f762f7ec4874c284f8aafe1ee8338db821308
Ben Xu <xuben.cn@gmail.com>
2018-07-26 15:53:59 +0800

    OTNode and its son class

---
+++ earth-commons/src/main/java/com/ani/commons/earth/domain/organization/tree/OrganizationUnit.java
@@ -0,0 +1,9 @@
+package com.ani.commons.earth.domain.organization.tree;
+
+
+/**
+ * Created by xuben on 18-7-26.
+ */
+public class OrganizationUnit extends OTNode {
+    private static final long serialVersionUID = 2408403406461033835L;
+}
