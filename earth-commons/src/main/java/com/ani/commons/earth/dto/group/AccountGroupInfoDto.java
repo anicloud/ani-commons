bc12ce33925e15aaff83690d4625695b6bc25083
Ben Xu <xuben.cn@gmail.com>
2018-07-26 22:02:45 +0800

    about group

---
+++ earth-commons/src/main/java/com/ani/commons/earth/dto/group/AccountGroupInfoDto.java
@@ -0,0 +1,83 @@
+package com.ani.commons.earth.dto.group;
+
+import com.ani.commons.earth.enumeration.GroupType;
+import com.ani.utils.dto.AniDto;
+
+import java.io.Serializable;
+
+/**
+ * Created by xuben on 18-7-26.
+ * Last Modified by xuben on 18-7-26.
+ */
+public class AccountGroupInfoDto extends AniDto implements Serializable {
+    private static final long serialVersionUID = -8616835316236418983L;
+
+    private Long groupId;
+
+    private Long ownerId;
+
+    private String name;
+
+    private String description;
+
+    private GroupType groupType;
+
+    public AccountGroupInfoDto(Long ownerId, String name, String description, GroupType groupType) {
+        this.ownerId = ownerId;
+        this.name = name;
+        this.description = description;
+        this.groupType = groupType;
+    }
+
+    public AccountGroupInfoDto(Long groupId, String name, String description) {
+        this.groupId = groupId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public AccountGroupInfoDto() {
+    }
+
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public GroupType getGroupType() {
+        return groupType;
+    }
+
+    public void setGroupType(GroupType groupType) {
+        this.groupType = groupType;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+}
+
+
--- earth-commons/src/main/java/com/ani/commons/earth/interfaces/AccountGroupService.java
+++ earth-commons/src/main/java/com/ani/commons/earth/interfaces/AccountGroupService.java
@@ -1,7 +1,8 @@
 package com.ani.commons.earth.interfaces;
 
 import com.ani.commons.earth.domain.account.AniAccountGroup;
-import com.ani.commons.earth.dto.info.AccountGroupProfileDto;
+import com.ani.commons.earth.dto.group.AccountGroupInfoDto;
+import com.ani.utils.exception.AniAuthException;
 import com.ani.utils.exception.AniDataException;
 import com.ani.utils.exception.AniRuleException;
 
@@ -13,14 +14,16 @@ public interface AccountGroupService {
 
     public AniAccountGroup findByGroupId(Long groupId) throws AniDataException;
 
-    public AniAccountGroup removeAccountsFromGroup(Long groupId, List<Long> accountsId) throws AniRuleException, AniDataException;
+    public void removeAccountsFromGroup(Long groupId, List<Long> accountsId) throws AniRuleException, AniDataException;
 
-    public AniAccountGroup addAccountsToGroup(Long groupId, List<Long> accountsId);
-
-    public AniAccountGroup modifyGroupProfile(AccountGroupProfileDto groupProfileDto);
-
-    public boolean removeGroupFromAccount(Long accountId, Long groupId);
+    public void addAccountsToGroup(Long groupId, List<Long> accountsId);
 
     public boolean clearGroup(Long accountId, Long groupId);
+
+    public void createGroup(AccountGroupInfoDto accountGroupInfoDto, Long opEmployeeId) throws AniRuleException, AniAuthException;
+
+    public boolean removeGroup(Long groupId, Long opEmployeeId) throws AniRuleException, AniAuthException;
+
+    public void modifyGroupProfile(AccountGroupInfoDto accountGroupInfoDto, Long opEmployeeId) throws AniRuleException, AniAuthException;
 
 }
