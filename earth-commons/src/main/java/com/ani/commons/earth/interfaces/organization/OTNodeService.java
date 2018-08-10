a84c12f65de849b850cc76008a83e1dc3b3b3c49
Ben Xu <xuben.cn@gmail.com>
2018-08-10 15:10:05 +0800

    OTNodeService

--- earth-commons/src/main/java/com/ani/commons/earth/interfaces/organization/OTNodeService.java
+++ earth-commons/src/main/java/com/ani/commons/earth/interfaces/organization/OTNodeService.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 /**
  * Created by zhanglina on 18-4-26.
- * Last Modified by xuben on 18-8-6.
+ * Last Modified by xuben on 18-8-10.
  */
 public interface OTNodeService {
 
@@ -25,33 +25,6 @@ public interface OTNodeService {
 
     public Set<Long> getDescendantNodes(Long nodeId, Long opAccountId) throws AniRuleException;
 
-
-//    public OTNode getTreeByEmployeeId(Long accountId) throws AniAuthException;
-
-//    public void addNodeMember(OTNodeMemberInfoDto otNodeMemberAddDto)throws AniAuthException,AniRuleException,AniDataException;
-//
-//    public Set<OTNodeBasicDto> getNodeChildren(OTNodeOpDto otNodeOpDto) throws AniAuthException;
-//
-//    public List<OTNodeMember> getNodeMembers(OTNodeOpDto otNodeOpDto) throws AniAuthException;
-//
-//    public void removeMember(OTNodeMemberOpDto removeDto,boolean isAll) throws AniAuthException;
-//
-//    public void updateMemberRole(OTNodeMemberDto otNodeMemberDto) throws AniAuthException;
-//
-//    public void updateMemberInfo(OTNodeMemberInfoDto otNodeMemberInfoDto) throws AniAuthException,AniDataException;
-//
-//   public OrgMember getNodeMemberByEmployeeId(OTNodeOpDto otNodeOpDto, Long employeeId) throws  AniAuthException,AniDataException;
-//
-//    public void updateEmployeeState(EmployeeStateUpdateDto employeeStateUpdateDto) throws AniAuthException;
-//
-//    public List<OrgMemberInfo> getMemberByNameAlp(String nameAlp, Integer orgId,Long nodeId);
-//
-//    public void addOrgMemberToNode(OTNodeOpDto otNodeOpDto,List<NodeAddMemberDto> nodeAddMemberDtos) throws AniDataException;
-//
-//    public void checkHaveRootPrivilege(Long opAccountId, Integer orgId) throws AniAuthException;
-//
-//    public void checkOrgNodeResourceAccessibility(OrgNodeAccessibilityDto orgResourceAccessDto) throws AniRuleException;
-//
-//    public void checkOrgResourceAccess(OrgResourceAccessDto orgResourceAccessDto) throws AniRuleException;
+    public Set<OTNodeBasicDto> getNodeChildren(Long nodeId, Long opAccountId) throws AniAuthException, AniRuleException;
 
 }
